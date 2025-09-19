5dcac485608b175d2287f6362842557a94443550
Davi Samuel Dalaroza <davi@changlee.com.br>
2025-09-18 21:12:48 -0300

    feat: add authentication functionality with login endpoint and JWT token generation

--- src/main/java/br/com/movieflix/service/UserService.java
+++ src/main/java/br/com/movieflix/service/UserService.java
@@ -3,15 +3,22 @@ package br.com.movieflix.service;
 import br.com.movieflix.entity.User;
 import br.com.movieflix.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 
